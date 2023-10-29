b9b2c7914e10a0d85630d860cf9c76bc7f609d67
Jacob Gardner <Jacob.gardner@bjss.com>
2023-10-29 10:53:58 +0000

    feat: complete typescript args processing

--- typescript/src/opts.ts
+++ typescript/src/opts.ts
@@ -10,6 +10,7 @@ export default function getOpts(): Opts {
     return cli([{
         name: "args",
         defaultOption: true,
+        multiple: true,
         type: String,
     }, {
         name: "config",
